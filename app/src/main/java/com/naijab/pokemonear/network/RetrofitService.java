b91449fa73a727ee6b89c7902d60f5458c247eb3
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-05 15:53:47 +0700

    +Fix, ServerTimeOutErrorException

--- app/src/main/java/com/naijab/pokemonear/network/RetrofitService.java
+++ app/src/main/java/com/naijab/pokemonear/network/RetrofitService.java
@@ -1,6 +1,7 @@
 package com.naijab.pokemonear.network;
 
-
+import android.support.annotation.RequiresApi;
+import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
@@ -8,23 +9,28 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitService {
 
-    public static final String BASE_URL = "http://sleepingforless.com:2222/";
-    private static Retrofit retrofit = null;
+  public static final String BASE_URL = "http://sleepingforless.com:2222/";
+  private static Retrofit retrofit = null;
 
-    public static Retrofit getRetrofit() {
+  @RequiresApi(9)
+  public static Retrofit getRetrofit() {
 
-        if (retrofit == null) {
+    if (retrofit == null) {
 
-            HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-            interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-            OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .connectTimeout(5, TimeUnit.MINUTES)
+          .readTimeout(5, TimeUnit.MINUTES)
+          .addInterceptor(interceptor)
+          .build();
 
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
-                    .client(client)
-                    .addConverterFactory(GsonConverterFactory.create())
-                    .build();
-        }
-        return retrofit;
+      retrofit = new Retrofit.Builder()
+          .baseUrl(BASE_URL)
+          .client(client)
+          .addConverterFactory(GsonConverterFactory.create())
+          .build();
     }
+    return retrofit;
+  }
 }
