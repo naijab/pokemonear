98d76eeb5e6199a85b194142978f80bcc6a23fbe
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-09 11:28:56 +0700

    +Add PokemonServerService interface

---
+++ app/src/main/java/com/naijab/pokemonear/network/PokemonServerService.java
@@ -0,0 +1,28 @@
+package com.naijab.pokemonear.network;
+
+import com.naijab.pokemonear.login.UserLoginModel;
+import com.naijab.pokemonear.maps.PokemonCatchableModel;
+import com.naijab.pokemonear.server.ServerStatusModel;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface PokemonServerService {
+
+  @GET("pokemon")
+  Call<ServerStatusModel> getServerStatus();
+
+  @FormUrlEncoded
+  @POST("pokemon/login")
+  Call<UserLoginModel> getUserLogin(@Field("email") String email,
+      @Field("password") String password);
+
+  @GET("pokemon/catchable")
+  Call<PokemonCatchableModel> getPokemon(@Header("pokemon_token") String token,
+      @Query("latitude") String latitude,
+      @Query("longitude") String longitude);
+}
