afa7202d6906e0999b485e44323df7cbf5cc5052
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-09 12:05:01 +0700

    +Add PokemonServerManager

---
+++ app/src/main/java/com/naijab/pokemonear/network/PokemonServerManager.java
@@ -0,0 +1,59 @@
+package com.naijab.pokemonear.network;
+
+import com.naijab.pokemonear.server.ServerStatusModel;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class PokemonServerManager {
+
+  private static PokemonServerManager pokemonServerManager;
+
+  public static PokemonServerManager getInstance() {
+    if (pokemonServerManager == null) {
+      pokemonServerManager = new PokemonServerManager();
+    }
+    return pokemonServerManager;
+  }
+
+  public void CheckServerStatus(final CheckServerStatusCallBack callBack) {
+    PokemonServerConnect.getInstance().getConnection().getServerStatus().enqueue(
+        new Callback<ServerStatusModel>() {
+          @Override
+          public void onResponse(Call<ServerStatusModel> call,
+              Response<ServerStatusModel> response) {
+
+            String ServerStatusMassage = response.body().getMessage();
+
+            if (response.isSuccessful()) {
+
+              if (ServerStatusMassage.equals("Server is active")) {
+
+                callBack.onServerActive(ServerStatusMassage);
+
+              } else {
+
+                callBack.onServerDown(ServerStatusMassage);
+
+              }
+            }
+          }
+
+          @Override
+          public void onFailure(Call<ServerStatusModel> call, Throwable t) {
+            callBack.onServerError(t.toString());
+          }
+        });
+  }
+
+  public interface CheckServerStatusCallBack {
+
+    void onServerActive(String ServerStatus);
+
+    void onServerDown(String ServerStatus);
+
+    void onServerError(String ServerError);
+  }
+
+
+}
