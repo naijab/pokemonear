50b76ff2c12fb2ff82e0e337b5463f7722b65390
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-09 12:47:40 +0700

    +Edit Refractor Code UserLoginManager

--- app/src/main/java/com/naijab/pokemonear/login/manager/UserLoginManager.java
+++ app/src/main/java/com/naijab/pokemonear/login/manager/UserLoginManager.java
@@ -3,36 +3,92 @@ package com.naijab.pokemonear.login.manager;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.widget.Toast;
-
-import com.naijab.pokemonear.network.RetrofitService;
-import com.naijab.pokemonear.network.ServiceInterface;
-import com.naijab.pokemonear.server.ServerStatusModel;
-
+import android.content.res.Resources;
+import com.naijab.pokemonear.R;
+import com.naijab.pokemonear.login.UserLoginModel;
+import com.naijab.pokemonear.network.PokemonServerConnect;
+import com.naijab.pokemonear.utility.Contextor;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class UserLoginManager {
 
-    private final String KEY_PREFS = "prefs_user";
-    private final String KEY_TOKEN = "token";
-    private final String KEY_EMAIL = "email";
+  private static UserLoginManager userLoginManager;
 
-    private SharedPreferences mPrefs;
-    private SharedPreferences.Editor mEditor;
+  private static final String KEY_PREFS = "prefs_user";
+  private static final String KEY_TOKEN = "token";
+  private static final String KEY_EMAIL = "email";
 
-    private boolean serverStatus = false;
-    Context context;
+  private SharedPreferences mPrefs;
+  private SharedPreferences.Editor mEditor;
 
-    public UserLoginManager(Context context){
-        mPrefs = context.getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE);
-        mEditor = mPrefs.edit();
+  public static UserLoginManager getInstance() {
+    if (userLoginManager == null) {
+      userLoginManager = new UserLoginManager();
     }
+    return userLoginManager;
+  }
 
-    public boolean saveUser(String token, String email) {
-        mEditor.putString(KEY_TOKEN, token);
-        mEditor.putString(KEY_EMAIL, email);
-        return mEditor.commit();
-    }
+  public boolean saveUser(String token, String email, Context context) {
+    mPrefs = context.getSharedPreferences(KEY_PREFS, Context.MODE_PRIVATE);
+    mEditor = mPrefs.edit();
+    mEditor.putString(KEY_TOKEN, token);
+    mEditor.putString(KEY_EMAIL, email);
+    return mEditor.commit();
+  }
+
+  public void CheckUserLogin(final String username, String password,
+      final UserLoginManagerCallBack callBack) {
+
+    PokemonServerConnect.getInstance().getConnection().getUserLogin(username, password).enqueue(
+        new Callback<UserLoginModel>() {
+          @Override
+          public void onResponse(Call<UserLoginModel> call, Response<UserLoginModel> response) {
+
+            if (response.isSuccessful()) {
+              if (response.body().getMessage().equals("Login Successful")) {
+                hideKeyboard();
+                String token = response.body().getToken();
+                boolean isSaveUser = saveUser(token, username, Contextor.getContext());
+
+                if (isSaveUser) {
+                  callBack.onUserLoginSuccess();
+                } else {
+                  callBack.onUserLoginUnableSave();
+                }
+              } else {
+                  callBack.onUserLoginInvalid();
+              }
+            } else {
+              callBack.onUserLoginFail();
+            }
+
+          }
+
+          @Override
+          public void onFailure(Call<UserLoginModel> call, Throwable t) {
+            callBack.onUserLoginError(t.getMessage());
+          }
+        });
+
+  }
+
+  private void hideKeyboard() {
+  }
+
+  public interface UserLoginManagerCallBack {
+
+    void onUserLoginSuccess();
+
+    void onUserLoginUnableSave();
+
+    void onUserLoginInvalid();
+
+    void onUserLoginFail();
+
+    void onUserLoginError(String errorMassage);
+  }
+
+
 }
