c3f7c67726c7b433ea54c17d3595ae71c3dba088
Nattapon Pondongnok <nattapon.jab@gmail.com>
2017-05-11 12:39:36 +0700

    +Add call back Pokemon result

--- app/src/main/java/com/naijab/pokemonear/maps/pokemon/PokemonManager.java
+++ app/src/main/java/com/naijab/pokemonear/maps/pokemon/PokemonManager.java
@@ -1,6 +1,8 @@
 package com.naijab.pokemonear.maps.pokemon;
 
 import com.naijab.pokemonear.network.PokemonServerConnect;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,41 +19,72 @@ public class PokemonManager {
     return pokemonManager;
   }
 
-  public void findPokemon(String token,
-                          String latitude,
-                          String longitude,
-                          final FindPokemonCallBack callBack){
+  private ArrayList<List<PokemonDataModel>> pokemonDataList;
+
+  public void findRandomPokemon(String token,
+      String latitude,
+      String longitude,
+      final FindPokemonCallBack callBack) {
 
     PokemonServerConnect.getInstance().getConnection().getPokemon(token,
         latitude,
-        longitude).enqueue(new Callback<List<PokemonCatchableModel>>() {
+        longitude).enqueue(new Callback<PokemonCatchableModel>() {
       @Override
-      public void onResponse(Call<List<PokemonCatchableModel>> call,
-          Response<List<PokemonCatchableModel>> response) {
-        if (response.isSuccessful()) {
-          callBack.onDetectPokemon();
+      public void onResponse(Call<PokemonCatchableModel> call,
+          Response<PokemonCatchableModel> response) {
+
+        if (response.isSuccessful()){
+          PokemonCatchableModel pokemonCatchableModel = response.body();
+          int pokemonDetectSize = pokemonCatchableModel.getData().size();
+
+          for (int i = 0; i < pokemonDetectSize ; i++) {
+//            pokemonDataList = new ArrayList<>(Arrays.asList(pokemonCatchableModel.getData()));
+            List<PokemonDataModel> list = pokemonCatchableModel.getData();
+            String pokemonID = list.get(i).getId();
+            String pokemonName = list.get(i).getName();
+            String pokemonNumber = list.get(i).getNumber();
+            double pokemonLatitude = list.get(i).getLatitude();
+            double pokemonLongitude = list.get(i).getLongitude();
+            long pokemonLife = list.get(i).getExpiration_timestamp();
+
+            callBack.onDetectPokemon(pokemonID,
+                pokemonName,
+                pokemonNumber,
+                pokemonLatitude,
+                pokemonLongitude,
+                pokemonLife,
+                pokemonDetectSize);
+          }
         }else {
-          callBack.onDetectPokemonFail();
+          String message = response.body().getMessage();
+          callBack.onDetectPokemonFail(message);
         }
-      }
-
-      @Override
-      public void onFailure(Call<List<PokemonCatchableModel>> call, Throwable t) {
-        callBack.onServerError(t.getMessage());
-      }
-    });
 
 
-  }
+    }
 
-  public interface FindPokemonCallBack{
+    @Override
+    public void onFailure (Call < PokemonCatchableModel > call, Throwable t){
+      callBack.onServerError(t.getMessage());
+    }
+  });
 
-    void onDetectPokemon();
+}
 
-    void onDetectPokemonFail();
+public interface FindPokemonCallBack {
 
-    void onServerError(String errorMassage);
-  }
+  void onDetectPokemon( String pokemonID,
+      String pokemonName,
+      String pokemonNumber,
+      double pokemonLatitude,
+      double pokemonLongitude,
+      long pokemonLife,
+      int pokemonCount);
+
+  void onDetectPokemonFail(String message);
+
+  void onServerError(String errorMassage);
+}
 
 
 }
